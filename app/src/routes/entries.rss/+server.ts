ee00dcb2660aeae1607e1dc2bb883a90115e737b
Rafael Gutkowski <raei@libred.org>
2023-11-18 15:50:17 +0300

    Add casts RSS feed

--- app/src/routes/entries.rss/+server.ts
+++ app/src/routes/entries.rss/+server.ts
@@ -1,25 +1,53 @@
+import { PUBLIC_BASE_GOERLI_ALCHEMY_API_KEY, PUBLIC_POSTER_CONTRACT } from '$env/static/public';
+import { EthercastPosterABI } from '$lib/ethercast.js';
 import { Feed } from 'feed';
+import { createPublicClient, http } from 'viem';
+import { baseGoerli } from 'viem/chains';
+
+function getContractEvents() {
+	const client = createPublicClient({
+		chain: baseGoerli,
+		transport: http(`https://base-goerli.g.alchemy.com/v2/${PUBLIC_BASE_GOERLI_ALCHEMY_API_KEY}`)
+	});
+
+	return client
+		.getContractEvents({
+			address: PUBLIC_POSTER_CONTRACT,
+			abi: EthercastPosterABI,
+			fromBlock: '0xBF2FD7',
+			event: 'NewPost'
+		})
+		.then((r) => {
+			return r.map((event) => {
+				return {
+					address: event.args.user,
+					content: event.args.content,
+					blockNumber: event.blockNumber
+				};
+			});
+		});
+}
 
 export async function GET(event) {
 	const entriesUrl = new URL('entries', event.request.url).toString();
 	const feed = new Feed({
-		title: 'Folklore',
+		title: 'Ethercast',
 		id: event.request.url,
 		link: event.request.url,
-		description: 'Recent entries from Folklore',
+		description: 'Recent casts from the ether',
 		language: 'en',
-		copyright: 'All rights reserved'
+		copyright: 'Public'
 	});
 
-	const entries = [];
+	const events = await getContractEvents();
 
-	entries.forEach((entry) => {
+	events.forEach((event) => {
 		feed.addItem({
-			guid: `${entriesUrl}#${entry.id}`,
-			title: entry.description || entry.id!,
-			author: [{ name: entry.contributor, email: entry.contributor }],
-			link: entry.url,
-			date: new Date(entry.created_at)
+			guid: `${event.address}@${event.blockNumber}`,
+			title: event.content,
+			author: [{ name: event.address, email: event.address }],
+			link: '#',
+			date: new Date()
 		});
 	});
 
