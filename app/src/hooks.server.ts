4638956af9f378983d2887fd7e333096124eb751
Rafael Gutkowski <raei@libred.org>
2023-11-18 18:49:56 +0300

    Remove (most) unused code

--- app/src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import { processAuthRequestEvent } from './lib/auth.server';
-
-export async function handle({ event, resolve }) {
-	processAuthRequestEvent(event);
-
-	return resolve(event);
-}
--- app/src/routes/+page.ts
+++ app/src/routes/+page.ts
@@ -1,8 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
-// it so that it gets served as a static asset in production
-export const prerender = true;
-
 export function load() {
 	throw redirect(302, '/submit');
 }
